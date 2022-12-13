d7023a1e22fef5c8da7aabf9a6091c5c685cbca0
Vlad Furman <117096112+FurmanVlad@users.noreply.github.com>
2022-12-13 17:51:52 +0200

    Add files via upload
    My project starting version

---
+++ MyPointment1/MyPointment/urls.py
@@ -0,0 +1,42 @@
+"""MyPointment URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from hospital import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name=''),
+
+    path('adminclick', views.adminclick_view),
+    path('doctorclick', views.doctorclick_view),
+    path('patientclick', views.patientclick_view),
+
+    path('adminsignup', views.admin_signup_view),
+    path('doctorsignup', views.doctor_signup_view,name='doctorsignup'),
+    path('patientsignup', views.patient_signup_view),
+    
+    path('adminlogin', LoginView.as_view(template_name='hospital/adminlogin.html')),
+    path('doctorlogin', LoginView.as_view(template_name='hospital/doctorlogin.html')),
+    path('patientlogin', LoginView.as_view(template_name='hospital/patientlogin.html')),
+
+
+
+
+]
