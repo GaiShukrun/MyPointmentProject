ad37a8ba06b572cef674e543deecbaf5ae2aa8b5
Gai <gaish@ac.sce.ac.il>
2022-12-16 11:48:07 +0200

    TextField Fixed,ya bitches

--- MyPointment/booking/views.py
+++ MyPointment/booking/views.py
@@ -52,14 +52,17 @@ def bookingSubmit(request):
     #Get stored data from django session:
     day = request.session.get('day')
     service = request.session.get('service')
-    syptoms =request.session.get('syptoms')
+    
+    
+    
+    
    
     #Only show the time of the day that has not been selected before:
     hour = checkTime(times, day)
     if request.method == 'POST':
         time = request.POST.get("time")
         date = dayToWeekday(day)
-        
+        syptoms = request.POST.get("freeform")
         
 
         if service != None:
@@ -75,6 +78,9 @@ def bookingSubmit(request):
                                 syptoms = syptoms,
 
                             )
+                            
+                            
+                            
                             messages.success(request, "Appointment Saved!")
                             return redirect('/')
                         else:
