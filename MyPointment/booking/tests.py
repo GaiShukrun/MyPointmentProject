5ead74ee9565de82873121921e5fe45d1dfc9a14
FurmanVlad <vladfurman98@gmail.com>
2022-12-16 13:40:52 +0200

    Added tests! (Sites is working, all data saves in database)

--- MyPointment/booking/tests.py
+++ MyPointment/booking/tests.py
@@ -1,19 +1,43 @@
-from django.test import TestCase
-from models import Appointment
+from django.test import TestCase, tag, Client
+from django.urls import reverse
+from django.http import HttpRequest, HttpResponse,response,request
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,get_user_model,get_user,user_logged_in
+from .models import Appointment
+import datetime
+
+
+# The test john = Appointment.objects.get(<field>) for ex:
+# john = Appointment.objects.get(day = "2022-12-18")
+# is valid, because if we will insert in <field> other date and not what we did in setUp(self), so it will give error.
+# EX: # john = Appointment.objects.get(day = "2022-12-25") - booking.models.Appointment.DoesNotExist: Appointment matching query does not exist.
 
 class AppointmentTestCase(TestCase):
   def setUp(self):
-    Appointment.objects.create(name="John Doe", time="2022-12-15 14:30:00")
-    Appointment.objects.create(name="Jane Smith", time="2022-12-16 10:00:00")
-    
-  def test_appointment_name(self):
-    john = Appointment.objects.get(name="John Doe")
-    jane = Appointment.objects.get(name="Jane Smith")
-    self.assertEqual(john.username, "John Doe")
-    self.assertEqual(jane.username, "Jane Smith")
-    
+    Appointment.objects.create(service = "Cardiologist" ,day = datetime.date(2022, 12, 18),time = "7:30 AM",syptoms = "Back pain")
+
+  def test_appointment_service(self):
+    john = Appointment.objects.get(service="Cardiologist")
+    self.assertEqual(john.service, "Cardiologist")
+
+  def test_appointment_day(self):
+    john = Appointment.objects.get(day = "2022-12-18")
+    self.assertEqual(john.day, datetime.date(2022, 12, 18))
+    # self.assertNotEqual(john.day, datetime.date(2022, 12, 18))
+
   def test_appointment_time(self):
-    john = Appointment.objects.get(name="John Doe")
-    jane = Appointment.objects.get(name="Jane Smith")
-    self.assertEqual(john.time, "2022-12-15 14:30:00")
-    self.assertEqual(jane.time, "2022-12-16 10:00:00")
+    john = Appointment.objects.get(time = "7:30 AM")
+    self.assertNotEqual(john.time, "3:30 PM")
+    self.assertEqual(john.time, "7:30 AM")
+
+  def test_appointment_syptoms(self):
+    # john = Appointment.objects.get(syptoms = "Leg pain")
+    # This will give error because booking.models.Appointment.DoesNotExist: Appointment matching query does not exist.
+    # so:
+    john = Appointment.objects.get(syptoms = "Back pain")
+    self.assertEqual(john.syptoms,"Back pain")
+    
+
+
+
+  
--- MyPointment/main/tests.py
+++ MyPointment/main/tests.py
@@ -1,10 +1,10 @@
-from django.test import TestCase
 from . import models, views, admin, forms
 from django.test import TestCase, tag, Client
 from django.urls import reverse
 from django.http import HttpRequest, HttpResponse,response,request
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,get_user_model,get_user,user_logged_in
+
 # Create your tests here.
 
 class SigninTest(TestCase):
